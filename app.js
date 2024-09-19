30162a83103fc90e520593a882b62cd9b3d999bc
Elijah Ajagbe <Elijah.Ajagbe@zenithbank.com>
2024-09-19 11:07:52 +0100

    Using express router

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
-const http = require('http');
-
 const express = require('express');
 
 const app = express();
 
-app.use((req, res, next) => {
-    console.log("In the middleware");
-    next();
-});
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-app.use((req, res, next) => {
-    console.log("In another middleware");
-    res.send('<h1>Hello from Express!</h1>');
-});
+
+app.use(adminRoutes);
+app.use(shopRoutes);
 
 app.listen(3000);
